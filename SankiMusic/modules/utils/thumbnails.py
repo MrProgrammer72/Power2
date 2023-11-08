89ce0f44470b3ca393d720d5d76acf426b904b63
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-11-08 11:08:40 +0530

    Update thumbnails.py

--- SankiMusic/modules/utils/thumbnails.py
+++ SankiMusic/modules/utils/thumbnails.py
@@ -130,7 +130,7 @@ async def gen_thumb(videoid, user_id):
         try:
             draw.text(
                 (450, 35),
-                f"HELL MUSIC BOT",
+                f"Harley Music Bot",
                 fill="white",
                 stroke_width=1,
                 stroke_fill="white",
